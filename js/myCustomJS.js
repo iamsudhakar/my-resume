2551f3528ac4cc37292bd1ed75032977ad24dd7a
Sudhakar <iamsudhakar49@gmail.com>
2021-01-07 19:53:28 +0530

    exp update

--- js/myCustomJS.js
+++ js/myCustomJS.js
@@ -5,7 +5,7 @@ function myFunction() {
 today = new Date()
 start = new Date(2014,06,04) // remember this is equivalent to 07 04 2010
 //dates in js are counted from 0, so 06 is july
-doj = new Date (2017,00,05)
+doj = new Date (2020,08,01)
 
 function mn(date1,date2) {
     var diff = Math.floor(date1.getTime() - date2.getTime());
@@ -16,11 +16,18 @@ function mn(date1,date2) {
 	var ndays = days-(365*y)
 	var m = Math.floor(ndays/30);
 
-	if (m == 0){
-		document.write(y +" years");
-	}else if(m == 1){
-		document.write(y +" years and " + m + " month");
+	if (y == 0){
+	}else if(y == 1){
+		document.write(y +" year and ");
 	}else{
-		document.write(y +" years and " + m + " months");
+		document.write(y +" years and ");
+	}
+	
+	if (m == 0){
+		document.write(m + " month");
+	}else if(m == 1){
+		document.write(m + " month");
+	}else{
+		document.write(m + " months");
 	}
 }
